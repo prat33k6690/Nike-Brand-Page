6c9ee2d2a9c792235cb2d00982068287613b63dc
prat33k6690 <106433208+prat33k6690@users.noreply.github.com>
2024-03-03 23:04:52 +0530

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,30 @@
+const HeroSection = () => {
+  return (
+    <main className="Hero-container">
+      <div className="hero-content">
+        <h1>YOUR FEET DESERVE THE BEST</h1>
+        <p>
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+          YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+        </p>
+        <div className="hero-btn">
+          <button>Shop Now</button>
+          <button>Category</button>
+        </div>
+                   <div className="shopping">Also Available On 
+                        <div className="brand-icons">
+                        <a href="https://www.amazon.in/s?k=nike+shoes&ref=nb_sb_ss_inft-rank-pairwise-override-in-t3_2_5"> <img src="/images/amazon.png" alt="amazon-logo"/></a>   
+                        <a href="https://www.flipkart.com/search?q=nike+shoes&sid=osp%2Ccil&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_1_5_sc_na_na&otracker1=AS_QueryStore_OrganicAutoSuggest_1_5_sc_na_na&as-pos=1&as-type=RECENT&suggestionId=nike+shoes%7CMen%27s+Footwear&requestId=dbc391d0-46f1-4cd7-8d80-23856afc99a6&as-backfill=on"><img src="/images/flipkart.png" alt="flipkart-logo"/></a>    
+
+                            <img src="" alt="" />
+                        </div>
+                   </div> 
+      </div>
+      <div className="hero-image">
+      <a href="https://www.nike.com/in/u/custom-nike-air-zoom-pegasus-flyease-by-you-10001267/2943873055"><img src="/images/shoe_image.png" alt="shoe_image"/></a>
+      </div>
+    </main>
+  );
+};
+
+export default HeroSection;
